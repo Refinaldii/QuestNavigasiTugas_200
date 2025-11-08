1730013de3fc62cba9ee822c413207b224d04fdb
Refinaldii <refinaldi9786@gmail.com>
2025-11-08 16:26:39 +0700

    membuat struktur dasar composable FormulirScreen dengan Scaffold

--- app/src/main/java/com/example/tugasnavigasiku/view/FormulirScreen.kt
+++ app/src/main/java/com/example/tugasnavigasiku/view/FormulirScreen.kt
@@ -1,2 +1,34 @@
 package com.example.tugasnavigasiku.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormulirScreen(navController: NavController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("Formulir Pendaftaran") })
+        },
+        bottomBar = {
+            Button(
+                onClick = { navController.navigate("welcome") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp)
+            ) { Text("Kembali ke Beranda") }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .padding(padding)
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            Text("Isi formulir pendaftaran di sini.")
+        }
+    }
+}
