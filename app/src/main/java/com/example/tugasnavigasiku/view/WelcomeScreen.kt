32db2cb30622e5d7f6f2ff743bb29adebecc8a0a
Refinaldii <refinaldi9786@gmail.com>
2025-11-08 16:22:49 +0700

    menambahkan Scaffold dan layout dasar pada WelcomeScreen

--- app/src/main/java/com/example/tugasnavigasiku/view/WelcomeScreen.kt
+++ app/src/main/java/com/example/tugasnavigasiku/view/WelcomeScreen.kt
@@ -1,9 +1,21 @@
 package com.example.tugasnavigasiku.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun WelcomeScreen(navController: NavController) {
-
+    Scaffold { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            // Konten utama akan ditambahkan nanti
+        }
+    }
 }
