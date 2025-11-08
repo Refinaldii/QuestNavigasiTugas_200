2dceb565841fbbd5bdabc72096a01ce8edaf7923
Refinaldii <refinaldi9786@gmail.com>
2025-11-08 16:20:51 +0700

    enambahkan setContent dengan TugasNavigasiKuTheme pada MainActivity

--- app/src/main/java/com/example/tugasnavigasiku/MainActivity.kt
+++ app/src/main/java/com/example/tugasnavigasiku/MainActivity.kt
@@ -2,9 +2,15 @@ package com.example.tugasnavigasiku
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.tugasnavigasiku.ui.theme.TugasNavigasiKuTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            TugasNavigasiKuTheme {
+            }
+        }
     }
 }
