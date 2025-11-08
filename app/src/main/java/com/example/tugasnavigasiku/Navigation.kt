19c88b6270c588ec50691024ce8da252e75574f7
Refinaldii <refinaldi9786@gmail.com>
2025-11-08 16:11:43 +0700

    menambahkan struktur dasar NavigationApp dengan NavHost dan NavController

--- app/src/main/java/com/example/tugasnavigasiku/Navigation.kt
+++ app/src/main/java/com/example/tugasnavigasiku/Navigation.kt
@@ -1,3 +1,18 @@
 package com.example.tugasnavigasiku
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
+@Composable
+fun NavigationApp() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "welcome"
+    ) {
+
+    }
+}
